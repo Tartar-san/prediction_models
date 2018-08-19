e5fe6247c3e178474d76a6b6620494d2ec8eca5e
anton.tarasov <Anton.Tarasov@eleks.com>
2018-08-19 16:28:58 +0300

    Added config, started crop script

---
+++ config.py
@@ -0,0 +1,16 @@
+dataset_paths = {
+    "train": "train",
+    "test": "test",
+    "submission_sample":  "sample_submission.csv",
+    "train_ship_segmentations": "train_ship_segmentations.csv"
+}
+
+# work
+server1_paths = {
+    "data_root": "/home/atarasov/datasets/airbus_challenge"
+}
+
+# ucu
+server2_paths = {
+
+}
